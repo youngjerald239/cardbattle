1c7c6870a0e52394f9538c8437889c67b910e6db
youngjerald239 <youngjerald239@gmail.com>
2022-08-14 11:45:45 -0500

    set up playersummary.js

---
+++ src/components/PlayerSummary/PlayerSummary.js
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+
+const red = '#821200'
+const blue = '#1953cb'
+
+
+export const PlayerSummary = ({main}) => {
+    return (
+    <div 
+      style={{backgroundColor: main ? red : blue}} 
+      className={styles.main}>Player Summary Component
+    </div>)
+}
