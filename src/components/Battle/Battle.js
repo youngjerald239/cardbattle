dfa48067fdfed266b15b7c1dde48a1e43a02d7cf
youngjerald239 <youngjerald239@gmail.com>
2022-08-14 13:16:04 -0500

    set player summary in battle.js

--- src/components/Battle/Battle.js
+++ src/components/Battle/Battle.js
@@ -1,17 +1,34 @@
 import { PlayerSummary } from 'components/PlayerSummary/PlayerSummary'
 import styles from './styles.module.css'
+import {useState} from 'react'
+import { playerStats, opponentStats } from 'components/shared'
 
 export const Battle = () => {
+
+    const [playerHealth, setPlayerHealth] = useState(playerStats.maxHealth)
+    const [opponentHealth, setOpponentHealth] = useState(opponentStats.maxHealth)
+
     return <div className={styles.main}>
         <div className={styles.main}>
             <div className={styles.opponent}>
                 <div className={styles.summary}>
-                    <PlayerSummary />
+                    <PlayerSummary 
+                        health={opponentHealth}
+                        name={opponentStats.name}
+                        level={opponentStats.level}
+                        maxHealth={opponentStats.maxHealth}
+                    />
                 </div>
             </div>
             <div className={styles.user}>
                 <div className={styles.summary}>
-                    <PlayerSummary main/>
+                    <PlayerSummary 
+                        main
+                        health={playerHealth}
+                        name={playerStats.name}
+                        level={playerStats.level}
+                        maxHealth={playerStats.maxHealth}    
+                     />
                 </div>
             </div>
         </div>
