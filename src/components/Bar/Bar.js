a2ead00bbcdd94a8e30dd8e400919356dbaf1e33
youngjerald239 <youngjerald239@gmail.com>
2022-08-14 12:23:59 -0500

    set props for health, level

---
+++ src/components/Bar/Bar.js
@@ -0,0 +1,5 @@
+import styles from './styles.module.css'
+
+export const Bar = () => {
+    return <div className={styles.main}></div>
+}
--- src/components/PlayerSummary/PlayerSummary.js
+++ src/components/PlayerSummary/PlayerSummary.js
@@ -1,13 +1,22 @@
+import {Bar} from 'components'
 import styles from './styles.module.css'
 
 const red = '#821200'
 const blue = '#1953cb'
 
 
-export const PlayerSummary = ({main = false}) => {
+export const PlayerSummary = ({main = false, name, level, health, maxHealth}) => {
     return (
     <div 
       style={{backgroundColor: main ? red : blue}} 
-      className={styles.main}>Player Summary Component
-    </div>)
+      className={styles.main}>
+      <div className={styles.info}>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.level}>Lvl: {level}</div>
+      </div>
+      <div className={styles.health}>
+          <Bar label="HP" value={health} maxHealth={maxHealth}/>
+      </div>
+    </div>
+    )
 }
