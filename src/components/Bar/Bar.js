db2157895f081439ba9f8973e64e9426ffd5b6ac
youngjerald239 <youngjerald239@gmail.com>
2022-08-14 12:52:56 -0500

    fixed error by import Bar

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -1,5 +1,5 @@
 import styles from './styles.module.css'
 
-export const Bar = () => {
+export const Bar = ({ label, health, maxHealth}) => {
     return <div className={styles.main}></div>
 }
--- src/components/PlayerSummary/PlayerSummary.js
+++ src/components/PlayerSummary/PlayerSummary.js
@@ -1,22 +1,28 @@
-import {Bar} from 'components'
-import styles from './styles.module.css'
+import { Bar } from '../Bar';
+import styles from './styles.module.css';
 
-const red = '#821200'
-const blue = '#1953cb'
+const red = '#821400';
+const blue = '#1953cb';
 
-
-export const PlayerSummary = ({main = false, name, level, health, maxHealth}) => {
-    return (
-    <div 
-      style={{backgroundColor: main ? red : blue}} 
-      className={styles.main}>
-      <div className={styles.info}>
-          <div className={styles.name}>{name}</div>
-          <div className={styles.level}>Lvl: {level}</div>
-      </div>
-      <div className={styles.health}>
-          <Bar label="HP" value={health} maxHealth={maxHealth}/>
-      </div>
+export const PlayerSummary = ({
+  main,
+  name,
+  level,
+  health,
+  maxHealth,
+}) => (
+  <div
+    className={styles.main}
+    style={{ backgroundColor: main ? red : blue }}
+  >
+    <div className={styles.info}>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.level}>Lvl {level}</div>
     </div>
-    )
-}
+
+    <div className={styles.health}>
+      <Bar label="HP" value={health} maxValue={maxHealth} />
+    </div>
+  </div>
+);
+
