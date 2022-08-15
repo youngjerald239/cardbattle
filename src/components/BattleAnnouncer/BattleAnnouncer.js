3b2003026db6b67d12446e4a462c62fea59096d6
youngjerald239 <youngjerald239@gmail.com>
2022-08-15 17:44:36 -0500

    created BattleAnnouncer component

---
+++ src/components/BattleAnnouncer/BattleAnnouncer.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export const BattleAnnouncer = ({ message }) => {
+  const typedMessage = useTypedMessage(message);
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.message}>{typedMessage}</div>
+    </div>
+  );
+};
