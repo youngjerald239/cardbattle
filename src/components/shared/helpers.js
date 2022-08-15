7e6f67d52de86607b0c08e7404b2284d92525d4d
youngjerald239 <youngjerald239@gmail.com>
2022-08-15 18:20:11 -0500

    set up ai for opponent

--- src/components/shared/helpers.js
+++ src/components/shared/helpers.js
@@ -3,3 +3,23 @@ export const wait = ms => new Promise((resolve => {
        resolve()
    }, ms) 
 }))
+
+export const attack = ({ attacker, receiver }) => {
+  const receivedDamage =
+    attacker.attack - (attacker.level - receiver.level) * 1.25;
+
+  const finalDamage = receivedDamage - receiver.defense / 2;
+
+  return finalDamage;
+}
+export const magic = ({ attacker, receiver }) => {
+  const receivedDamage =
+    attacker.magic - (attacker.level - receiver.level) * 1.25;
+
+  const finalDamage = receivedDamage - receiver.magicDefense / 2;
+
+  return finalDamage;
+}
+export const heal = ({ receiver }) => {
+  return receiver.magic + receiver.level * 0.25;
+}
