4fbb37ac88662d6424648f23030d52c558aa3b2e
youngjerald239 <youngjerald239@gmail.com>
2022-08-15 20:10:27 -0500

    set battle animation

--- src/hooks/useBattleSequence.js
+++ src/hooks/useBattleSequence.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react'
 import { playerStats, opponentStats } from 'components/shared'
 import { wait } from '@testing-library/user-event/dist/utils'
 
-export const useBattleSequence = () => {
+export const useBattleSequence = sequence => {
 
     const [turn, setTurn] = useState(0)
     const [inSequence, setInSequence] = useState(false)
@@ -35,12 +35,37 @@ export const useBattleSequence = () => {
                         ? setPlayerAnimation('attack') 
                         : setOpponentAnimation('attack')
                         await wait(100)
+
+                       turn === 0
+                        ? setPlayerAnimation('static')
+                        : setOpponentAnimation('static');
+                        await wait(500);
+
+                       turn === 0
+                        ? setOpponentAnimation('damage')
+                        : setPlayerAnimation('damage');
+                        await wait(750);
+
+                       turn === 0
+                        ? setOpponentAnimation('static')
+                        : setPlayerAnimation('static');
+                       setAnnouncerMessage(`${receiver.name} felt that!`);
+                       turn === 0
+                        ? setOpponentHealth(h => (h - damage > 0 ? h - damage : 0))
+                        : setPlayerHealth(h => (h - damage > 0 ? h - damage : 0)); // We don't want a negative HP.
+                        await wait(2000);
+
+                        setAnnouncerMessage(`Now it's ${receiver.name} turn!`);
+                        await wait(1500);
+
+                        setTurn(turn === 0 ? 1 : 0);
+                        setInSequence(false);
                     })()
-                break
+                    break
 
-                default:
+                    default:
 
-                break
+                    break
             }
         }
     }, [sequence])
